fe95771e1407195503d30f1e168b9eec10261c33
tyson717 <111126837+tyson717@users.noreply.github.com>
2022-11-03 10:31:15 +0300

    Initial commit

---
+++ 08_Assignment.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int x;
+    int y;
+
+    // x = 4;
+    //y=4;
+    x = y = 4;
+    //   x = (y=4);
+
+    printf("x= %d , y= %d\n", x, y);
+
+    int z;
+    z = 5;
+    //5=z ;
+    printf("z = %d",z);
+    return 0;
+}
